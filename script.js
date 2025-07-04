684fdddcc7e3b725ea002dfb428f3331d8cd532f
dheerajjha97 <74364669+dheerajjha97@users.noreply.github.com>
2025-07-04 13:16:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,4 +2,5 @@
 document.getElementById('admissionForm').addEventListener('submit', function(e) {
   e.preventDefault();
   alert('फॉर्म सफलतापूर्वक सबमिट किया गया!');
+  // आप यहां localStorage या PDF export की सुविधा जोड़ सकते हैं
 });
