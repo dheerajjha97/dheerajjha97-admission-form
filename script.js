bb9753cd62d29df407ff32a49bb05dd2f2dd5495
dheerajjha97 <74364669+dheerajjha97@users.noreply.github.com>
2025-07-04 17:36:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-
-document.getElementById("classSelect").addEventListener("change", function() {
-  const subjectSection = document.getElementById("subjectSection");
-  subjectSection.style.display = this.value === "11" ? "block" : "none";
-});
-
-document.getElementById("admissionForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("फॉर्म सफलतापूर्वक सबमिट हुआ!");
-});
+console.log("School Admission App Loaded");
